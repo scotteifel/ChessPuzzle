44e48ea3df713a568b2884e7e5348818a502d9f6
Scott <scott.eifel@hotmail.com>
2020-06-12 00:44:32 -0400

    adding gui for solving algorithm

---
+++ gui.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+
+
+class Program(tk.Frame):
+
+    def __init__(self,parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        row, col = 0, 0
+        background = 'brown'
+        for x in range(8):
+            for y in range(8):
+                self.row = tk.Label(self.parent, text="Space", padx=10,pady=20,
+                relief="groove",background=background)
+                self.row.grid(row=row,column=col)
+                col += 1
+                if background == 'brown':
+                    background = 'white'
+                else:
+                    background = 'brown'
+            if background == 'brown':
+                background = 'white'
+            else:
+                background = 'brown'
+            col = 0
+            row += 1
+
+
+root = tk.Tk()
+root.title("Chessmaster")
+
+app = Program(root)
+
+
+root.mainloop()
