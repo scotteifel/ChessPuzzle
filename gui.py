f7a410ddfc5d13a18101bf2ac8c4c9f498cb9f86
Scott <scott.eifel@hotmail.com>
2020-06-24 14:34:27 -0400

    Didn't need downwards diagonal func, fixed rbound range

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
-import tkinter as tk
-import time
 
-import threading
+import tkinter as tk
+from tkinter import messagebox
+import time
 
 board = [
           [0,0,0,0,0,0,0,0],
@@ -18,19 +18,17 @@ global XZ
 NEG = []
 XZ = 0
 
-
 class Program:
     def __init__(self,parent):
-        self.parent = parent
 
+        self.parent = parent
+        self.vars = []
         row, col = 0, 0
         background = 'brown'
-        self.vars = []
 
         for x in range(8):
             for y in range(8):
 
-                # var =  "var" + str(row)+str(col)
                 var = tk.StringVar()
                 self.vars.append(var)
                 self.row = tk.Label(self.parent,width=11,pady=25,
@@ -38,6 +36,7 @@ class Program:
                                     textvariable=var,font="bold")
                 self.row.grid(row=row,column=col)
                 col += 1
+                #Draws the chessboard
                 if background == 'brown':
                     background = 'white'
                 else:
@@ -49,15 +48,16 @@ class Program:
             col = 0
             row += 1
 
-        self.state = tk.Button(self.parent,text="Start",
+        self.state = tk.Button(self.parent,text="Solve", pady=3,
                                command = self.start_solving)
-        self.state.grid(row=9,column=4)
+        self.state.grid(row=10,column=3, columnspan=2)
 
 
+    #A new function is created to pass in 0 for first iteration
     def start_solving(self):
-        print("Start solving")
         self.solve(board,0)
-
+        ok = messagebox.askokcancel(
+        message="Complete")
 
     def place_queens(self,*args):
 
@@ -79,8 +79,6 @@ class Program:
 
 
     def solve(self, b, r):
-        this = str(self.vars[9])
-
         global XZ
         global NEG
         queen_total = 0
@@ -132,61 +130,30 @@ class Program:
             if x[col] == 1:
                 return True
 
-        ##row by row, checks the upwards and downwards diagonal spaces.
-        upwards = row-1
         #checks if the row is the first one
-        if upwards != -1:
-            if self.upwards_diagonals(b, col,lowest=upwards):
-                return True
-
-        downwards = row+1
-        #check if the row is last one
-        if downwards != 7:
-            if self.downwards_diagonals(b, col,highest=downwards):
+        if row != 0:
+            #count the rows moving upwards starting
+            #on the above row
+            if self.upwards_diagonals(b, col,row-1):
                 return True
 
         return False
 
 
-    def upwards_diagonals(self, b, column, lowest=6):
-            #variables will count both to the right and left
+    def upwards_diagonals(self, b, column, row):
+            #vars will count to the right and left
             r_bound = column+1
             l_bound = column-1
-            #count rows moving upwards starting at the row above
-            #the current one
-            for _ in range(lowest,-1,-1):
-                if r_bound < 7:
+
+            for _ in range(row,-1,-1):
+                if r_bound < 8:
                     if b[_][r_bound] == 1:
                         return True
-
                 r_bound += 1
+
                 if l_bound > -1:
                     if b[_][l_bound] == 1:
                         return True
-                l_bound -= 1
-            return False
-
-
-    #seeks diagonals going down
-    def downwards_diagonals(self, b, column, highest=1):
-            #variables will count both to the right and left
-            l_bound = column
-            r_bound = column
-            #count rows downwards starting at the row below the current one
-            for _ in range(highest,8):
-                #Using try/except to return True
-                #when a coord hits the the side of the board
-                try:
-                    if b[_][r_bound+1] == 1:
-                        return True
-                except:
-                    pass
-                try:
-                    if b[_][l_bound-1] == 1:
-                        return True
-                except:
-                    pass
-                r_bound += 1
                 l_bound -= 1
             return False
 
@@ -202,7 +169,8 @@ class Program:
     def queen_points(self, b):
         #Returns the spaces containing a queen
         count = []
-        x=0
+        x = 0
+
         for row in range(8):
             for col in range(8):
                 if b[row][col] == 1:
@@ -215,11 +183,8 @@ class Program:
             print(row)
 
 
-#End of tkinter class
-
-
 root = tk.Tk()
-root.title("Chessmaster")
+root.title("8 Queens")
 
 app = Program(root)
 
